5cdffe1d5dd262bce9b1b1a52e43c486d95fe1d0
Daniel L Wang <46355940+daniel-awake@users.noreply.github.com>
2021-01-13 12:20:50 -0800

    Adjust timeout for iwlwifi for ap beacons to reduce connection dropouts.
    https://bugzilla.kernel.org/show_bug.cgi?id=203709

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -92,7 +92,7 @@
 /* RSSI offset for WkP */
 #define IWL_RSSI_OFFSET 50
 #define IWL_MVM_MISSED_BEACONS_THRESHOLD 8
-#define IWL_MVM_MISSED_BEACONS_THRESHOLD_LONG 16
+#define IWL_MVM_MISSED_BEACONS_THRESHOLD_LONG 128
 
 /* A TimeUnit is 1024 microsecond */
 #define MSEC_TO_TU(_msec)	(_msec*1000/1024)
